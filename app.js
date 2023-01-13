c622bab627b80448dfc132e028f6e3a0aa8366f5
Andrew <fredickandrewdimo@gmail.com>
2023-01-13 19:24:57 +0800

    Added sample task and live date

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Andrew");
+document.addEventListener("DOMContentLoaded", function () {
+  function dateToday() {
+    const today = new Date();
+    const day = today.getDay();
+    const date = today.getDate();
+    const month = today.getMonth();
+
+    var days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    var months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const dateTodayEl = document.getElementById("date-today");
+    dateTodayEl.innerText = `${days[day]}, ${date} ${months[month]}`;
+  }
+  dateToday();
+});
